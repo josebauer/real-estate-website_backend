7bae534285fe84ac8cfcd526920bf53d79e620f0
José Bauer <jose.h.bauer03@gmail.com>
2024-06-05 15:09:19 -0300

    Adiciona endpoint de registro de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { realEstateController } from './controllers/realEstateController'
+import { authController } from './controllers/authController'
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
@@ -11,5 +14,6 @@ router.get('/real-estate/featured', realEstateController.featured)
 router.get('/real-estate/newest', realEstateController.newest)
 router.get('/real-estate/filter', realEstateController.filter)
 router.get('/real-estate/:id', realEstateController.show)
+
 
 export { router } 
