0a6e543a187ca36d41d5f080a663ac48b32a1cb5
José Bauer <jose.h.bauer03@gmail.com>
2024-06-12 11:54:57 -0300

    Adiciona middleware de autenticação

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtService } from "../services/jwtService";
+import { userService } from "../services/userService";
+import { JwtPayload } from "jsonwebtoken";
+import { UserInstance } from "../models/User";
+
+export interface AuthenticatedRequest extends Request {
+  user?: UserInstance | null
+}
+
+export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const authorizationHeader = req.headers.authorization
+
+  if (!authorizationHeader) return res.status(401).json({
+    message: 'Não autorizado! Nenhum token foi encontrado.'
+  })
+
+  const token = authorizationHeader.replace(/Bearer /, '')
+
+  jwtService.verifyToken(token, (error, decoded) => {
+    if (error || typeof decoded === undefined) return res.status(401).json({
+      message: 'Não autorizado! Token inválido.'
+    })
+
+    userService.findByEmail((decoded as JwtPayload).email).then(user => {
+      req.user = user
+      next()
+    })
+  })
+}
