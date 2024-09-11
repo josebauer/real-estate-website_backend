513ebdaf93b114780f372527eb0a11883305c958
José Bauer <jose.h.bauer03@gmail.com>
2024-09-11 17:53:57 -0300

    Adiciona o métodos para gerar um código de redefinição de senha e um para verificar o código no userService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,6 @@
 import { User } from "../models"
 import { UserCreationAttributes } from "../models/User"
+import crypto from 'crypto';
 
 export const userService = {
   findByEmail: async (email: string) => {
@@ -35,5 +36,25 @@ export const userService = {
     })
 
     return updatedUsers[0]
+  },
+
+  generateResetCode: async (userId: number) => {
+    const resetCode = crypto.randomInt(100000, 999999).toString()
+    const expirationDate = new Date(Date.now() + 10 * 60 * 1000)
+
+    const [affectedRows, updatedUsers] = await User.update(
+      { resetCode, resetCodeExpires: expirationDate },
+      { where: { id: userId }, returning: true }
+    )
+
+    return resetCode
+  },
+
+  verifyResetCode: async (email: string, code: string) => {
+    const user = await User.findOne({ where: { email, resetCode: code } })
+    if (!user || !user.resetCodeExpires || new Date() > user.resetCodeExpires) {
+      throw new Error('Código inválido ou expirado.')
+    }
+    return user
   }
 }
