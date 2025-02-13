ff6fd9842b84aa2a0d8ee79ed273d00bb05af00f
José Bauer <jose.h.bauer03@gmail.com>
2025-02-13 10:58:19 -0300

    Cria service de Schedule e associação de tabelas
    - Cria service com funções para listar, criar, excluir e alterar o status dos agendamentos;
    - Cria associação entre as tabelas Schedule, User e Real Estate.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,23 +1,34 @@
 import { Category } from "./Category";
 import { Favorite } from "./Favorite";
 import { RealEstate } from "./RealEstate";
+import { Schedule } from "./Schedule";
 import { User } from "./User";
 
+// Associação de categorias com imóveis
 Category.hasMany(RealEstate, { as: 'realEstate' })
-
 RealEstate.belongsTo(Category)
+
+// Associação de imóveis com favoritos
 RealEstate.belongsToMany(User, { through: Favorite })
 RealEstate.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'real_estate_id' })
 
+// Associação de usuários com favoritos
 Favorite.belongsTo(RealEstate)
 Favorite.belongsTo(User)
-
 User.belongsToMany(RealEstate, { through: Favorite })
 User.hasMany(Favorite, { as: 'favoritesRealEstate', foreignKey: 'user_id' })
+
+// Associação de agendamentos
+User.hasMany(Schedule, { as: 'schedules', foreignKey: 'user_id' });
+RealEstate.hasMany(Schedule, { as: 'schedules', foreignKey: 'real_estate_id' });
+
+Schedule.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
+Schedule.belongsTo(RealEstate, { as: 'realEstate', foreignKey: 'real_estate_id' });
 
 export {
   Category,
   RealEstate,
   Favorite,
-  User
+  User,
+  Schedule
 }
---
+++ src/services/scheduleService.ts
@@ -0,0 +1,71 @@
+import { RealEstate } from "../models";
+import { Schedule } from "../models/Schedule";
+
+
+export const scheduleService = {
+  findByUserId: async (userId: number) => {
+    const schedules = await Schedule.findAll({
+      where: { userId },
+      include: {
+        model: RealEstate,
+        as: "realEstate",
+        attributes: [
+          "id",
+          "title",
+          "price",
+          "negotiation",
+          "district",
+          "city",
+          "state",
+          ["building_area", "buildingArea"],
+          ["total_area", "totalArea"],
+          "bedrooms",
+          "suites",
+          "garages",
+          ["images_url", "imagesUrl"],
+        ],
+      },
+    });
+
+    return {
+      userId,
+      schedules: schedules.map((schedule) => ({
+        id: schedule.id,
+        realEstate: schedule.realEstateId,
+        scheduledDate: schedule.scheduledDate,
+        status: schedule.status,
+      })),
+    };
+  },
+
+  create: async (userId: number, realEstateId: number, scheduledDate: Date) => {
+    const schedule = await Schedule.create({
+      userId,
+      realEstateId,
+      scheduledDate,
+      status: "pendente"
+    });
+
+    return schedule;
+  },
+
+  delete: async (userId: number, scheduleId: number) => {
+    await Schedule.destroy({
+      where: {
+        id: scheduleId,
+        userId,
+      },
+    });
+  },
+
+  updateStatus: async (scheduleId: number, status: "pendente" | "confirmado" | "cancelado") => {
+    const schedule = await Schedule.findByPk(scheduleId);
+
+    if (!schedule) throw new Error("Agendamento não encontrado.");
+
+    schedule.status = status;
+    await schedule.save();
+
+    return schedule;
+  },
+};
